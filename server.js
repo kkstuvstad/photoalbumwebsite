572704d2245d51c2175a5955aa65cdd9bd15e0af
kkstuvstad <stuvstak@oregonstate.edu>
2016-12-12 15:57:28 -0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,51 @@
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express();
+var photoData = require('./photos-data');
+var port = process.env.PORT || 3000;
+
+// Use Handlebars as the view engine for the app.
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Listen on the specified port.
+app.listen(port, function () {
+  console.log("== Listening on port", port);
+});
+
+//Displays home page
+app.get('/', function (req, res) {
+  res.render('index-page', {
+    pageTitle: 'Welcome!',
+    photoData: photoData
+  });
+});
+
+//Displays list of sticky notes for each person from drop-down menu
+ app.get('/photos/:location', function (req, res, next) {
+
+   var location = req.params.user;
+   var place = usersData[location];
+
+   if (user) {
+     res.render('photos-page', {
+       pageTitle: 'ToDoIt - ' + location,
+       photoName: location.place,
+       photos: location.photos
+     });
+   } else {
+     // Goes to 404 function if the requested user does not exist
+     next();
+   }
+ });
+
+// Displays 404 error message
+app.get('*', function(req, res) {
+  res.status(404).render('404-page', {
+    pageTitle: '404'
+  });
+});
